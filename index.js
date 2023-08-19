dd91b45f2294cfd8969a4d6cf2dcb47081c736ba
Darren Candra <darren@candra.dev>
2023-08-20 03:19:35 +1000

    Improved find with modes (contains, prefix and matches).

--- index.js
+++ index.js
@@ -65,31 +65,77 @@ text.addEventListener("keydown", function (event) {
 });
 
 const searchInput = document.getElementById('searchInput');
+const modeDropdown = document.querySelector('.mode-dropdown');
 const resultsContainer = document.querySelector('.results');
 const spotlight = document.querySelector('.spotlight');
-
 let isSearchOpen = false;
+let selectedMode = 'contains:';
+
+// Handle dropdown selection using arrow keys and Enter
+let dropdownItems = document.querySelectorAll('.dropdown-item');
+let selectedDropdownItem = 0;
+
+function updateDropdownSelection() {
+    for (let i = 0; i < dropdownItems.length; i++) {
+        if (i === selectedDropdownItem) {
+            dropdownItems[i].classList.add('selected');
+        } else {
+            dropdownItems[i].classList.remove('selected');
+        }
+    }
+}
 
 searchInput.addEventListener('input', () => {
-    const searchText = searchInput.value.toLowerCase();
+    const searchText = searchInput.value;
     updateResults(searchText);
 
+    if (!searchText.includes(':')) {
+        modeDropdown.style.display = 'block';
+    } else {
+        modeDropdown.style.display = 'none';
+    }
 });
 
-document.addEventListener("keydown", function (event) {
-    if (((event.ctrlKey || event.metaKey) && event.key === "f") && text.value !== "") {
-            if (isSearchOpen) {
-                closeSearch();
-            } else {
-                openSearch();
-            }
+document.addEventListener('keydown', (event) => {
+    if ((event.ctrlKey || event.metaKey) && event.key === 'f') {
+        event.preventDefault();
+
+        if (isSearchOpen) {
+            closeSearch();
+        } else {
+            openSearch();
         }
+    }
+
+    if (event.key === 'Escape') {
+        event.preventDefault();
+        closeSearch();
+    }
+
+    if (isSearchOpen && event.key === 'ArrowDown') {
+        event.preventDefault();
+        selectedDropdownItem = (selectedDropdownItem + 1) % dropdownItems.length;
+        updateDropdownSelection();
+        selectedMode = dropdownItems[selectedDropdownItem].dataset.mode;
+        searchInput.value = selectedMode;
+        updateResults(searchInput.value);
+    }
+
+    if (isSearchOpen && event.key === 'ArrowUp') {
+        event.preventDefault();
+        selectedDropdownItem = (selectedDropdownItem - 1 + dropdownItems.length) % dropdownItems.length;
+        updateDropdownSelection();
+        selectedMode = dropdownItems[selectedDropdownItem].dataset.mode;
+        searchInput.value = selectedMode;
+        updateResults(searchInput.value);
+    }
 });
 
 function openSearch() {
     isSearchOpen = true;
     spotlight.classList.add('active');
     searchInput.focus();
+    modeDropdown.style.display = 'block';
 }
 
 function closeSearch() {
@@ -97,33 +143,46 @@ function closeSearch() {
     searchInput.value = '';
     resultsContainer.innerHTML = '';
     spotlight.classList.remove('active');
+    modeDropdown.style.display = 'none';
 }
 
 function updateResults(searchText) {
     resultsContainer.innerHTML = '';
 
+    const searchMode = getSearchMode(searchText);
+    const searchTerm = extractSearchTerm(searchText);
     const textContent = text.value.toLowerCase();
-    const phrases = textContent.split('\n'); // Split by newlines
+    const words = textContent.split(/\s+/);
 
-    if (textContent !== "" || "word:") {
-        var matchingPhrases = phrases.filter(phrase => {
-            if (searchText.startsWith('word:')) {
-                const wordToSearch = searchText.substring(5); // Remove 'word:' prefix
-                return phrase.match(`/\b${searchText}\b/`).length !== 0
-            } else {
-                return phrase.includes(searchText);
-            }
-        });
+    let matchingWords;
+
+    if (searchInput.value !== "") {
+        if (searchMode === 'contains:') {
+            matchingWords = words.filter(word => word.includes(searchTerm.toLowerCase()));
+        } else if (searchMode === 'prefix:') {
+            matchingWords = words.filter(word => word.startsWith(searchTerm.toLowerCase()));
+        } else if (searchMode === 'matches:') {
+            matchingWords = words.filter(word => word === searchTerm.toLowerCase());
+        }
     }
 
-    if (matchingPhrases.length > 0) {
-        matchingPhrases.forEach(phrase => {
+    if (matchingWords.length > 0) {
+        matchingWords.forEach(word => {
             const resultItem = document.createElement('div');
             resultItem.classList.add('result-item');
-            resultItem.textContent = phrase;
+            resultItem.textContent = word;
             resultsContainer.appendChild(resultItem);
         });
     } else {
-        resultsContainer.innerHTML = '<p>No matching phrases found</p>';
+        resultsContainer.innerHTML = '<p>No matching words found.</p>';
     }
+}
+
+function getSearchMode(searchText) {
+    const searchModes = ['contains:', 'prefix:', 'matches:'];
+    return searchModes.find(mode => searchText.toLowerCase().startsWith(mode));
+}
+
+function extractSearchTerm(searchText) {
+    return searchText.split(':').slice(1).join(':').trim();
 }
